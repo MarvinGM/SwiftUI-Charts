064458216658895850c78bb7b328c7ed0c28b79b
MarvinGM <marvinmahilum091197@gmail.com>
2021-06-05 08:50:43 +0800

    Initial Commit

---
+++ SwiftUI Charts/SwiftUI_ChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ChartsApp.swift
+//  SwiftUI Charts
+//
+//  Created by Amben on 6/5/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
